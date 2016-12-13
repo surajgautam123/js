7d84b017833dde1b192e2b34be1916e709eb9f0d
Rupsingh Chouhan <Rupsingh Chouhan>
2016-12-13 12:53:51 +0530

    2 New fill added

--- object/rupesh.js
+++ object/rupesh.js
@@ -1,1 +1,14 @@
-testing
+var marutiCar = {
+	model: '2019',
+	color: 'black',
+	brand: 'wegonR'
+	car: 
+	speciFications{		
+		seathight: '4ft',
+		seatwidth: '2ft',
+		seatcolor: 'gray',
+	}
+	carModels{		
+		modelName: 'xs', 'xii',
+	}
+}
